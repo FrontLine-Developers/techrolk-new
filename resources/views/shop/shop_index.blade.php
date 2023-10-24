1e149c05c3099d4cfdd82da4bcc88ceb0783ff32
nelitha13 <nelithavindinu7@gmail.com>
2023-10-24 13:53:45 +0530

    p

--- resources/views/shop/shop_index.blade.php
+++ resources/views/shop/shop_index.blade.php
@@ -6,7 +6,7 @@
 <meta http-equiv="cache-control" content="max-age=604800" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<title>Shoppp │ TechRoLK Engineering Solutions</title>
+<title>Shop │ TechRoLK Engineering Solutions</title>
 
 
 
